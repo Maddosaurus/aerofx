95c0105bfacbcecef1dcfca98ca96e9d58da1c3f
Matthias <me@sigpwr.de>
2014-06-04 21:37:18 +0200

    Fine-tuning of font-size and RadioButton-Components Reworked imports.

--- src/main/java/com/aerofx_project/demo/Tester.java
+++ src/main/java/com/aerofx_project/demo/Tester.java
@@ -33,9 +33,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
