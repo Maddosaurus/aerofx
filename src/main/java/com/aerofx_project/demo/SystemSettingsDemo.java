e8e49bbc97115e2466add1545cc55a6b16010d84
Matthias <me@sigpwr.de>
2014-06-19 18:29:59 +0200

    Checkbox-Mark implemented. Overhaul of RadioButton > radio > dot Fine-tuned layout of SystemSettingsDemo.

--- src/main/java/com/aerofx_project/demo/SystemSettingsDemo.java
+++ src/main/java/com/aerofx_project/demo/SystemSettingsDemo.java
@@ -34,6 +34,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.scenicview.ScenicView;
 
 /**
